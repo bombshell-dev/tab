ed974b9ca3b709f2db92665363b334b9820b52ff
AmirSa12 <amirhosseinpr184@gmail.com>
2024-10-28 19:02:56 +0330

    feat: tests

---
+++ tests/cli.test.ts
@@ -0,0 +1,35 @@
+import { exec } from "child_process";
+import { describe, it, expect } from "vitest";
+
+function runCommand(command: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(stderr);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+describe("CLI Completion Tests", () => {
+  it("should complete vite commands", async () => {
+    const output = await runCommand("pnpm tsx demo.ts complete --");
+    console.log("Command Output:", output);
+    expect(output).toContain("src/");
+    expect(output).toContain("./");
+    // expect(output).toContain('--base');
+  });
+
+  it("should complete options", async () => {
+    const output = await runCommand("pnpm tsx demo.ts complete -- --");
+    console.log("Command Output:", output);
+    expect(output).toContain("--port");
+    expect(output).toContain("--config");
+    expect(output).toContain("--base");
+    expect(output).toContain("--logLevel");
+    expect(output).toContain("--filter");
+    expect(output).toContain("--mode");
+  });
+});
