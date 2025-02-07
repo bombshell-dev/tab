c0ede2fd362ffbf61fa71459f3cb3c02aadf8fb4
AmirSa12 <amirhosseinpr184@gmail.com>
2025-02-07 21:33:06 +0330

    update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/citty.ts', 'src/cac.ts'],
-  format: ['esm'],
-  dts: true,
-  clean: true,
+    entry: ['src/index.ts', 'src/citty.ts', 'src/cac.ts'],
+    format: ['esm'],
+    dts: true,
+    clean: true,
 });
