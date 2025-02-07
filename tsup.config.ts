731e50d10060c22df1f2d44e9185412ef005dbc9
AmirSa12 <amirhosseinpr184@gmail.com>
2025-02-07 23:33:59 +0330

    format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/index.ts', 'src/citty.ts', 'src/cac.ts'],
-    format: ['esm'],
-    dts: true,
-    clean: true,
-    skipNodeModulesBundle: true,
-    esbuildOptions(options) {
-        options.loader = { '.ts': 'ts' };
-    },
+  entry: ['src/index.ts', 'src/citty.ts', 'src/cac.ts'],
+  format: ['esm'],
+  dts: true,
+  clean: true,
+  skipNodeModulesBundle: true,
+  esbuildOptions(options) {
+    options.loader = { '.ts': 'ts' };
+  },
 });
